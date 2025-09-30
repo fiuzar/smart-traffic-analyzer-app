78b71c43aed3b314e5e42d1d9b9e8c9f2cd6a31f
Amoke Emmanuel <emmanuelamoke35@gmail.com>
2025-09-30 14:21:41 +0100

    version 0.10.5

--- src/app/(prepage)/docs/[end_point]/layout.jsx
+++ src/app/(prepage)/docs/[end_point]/layout.jsx
@@ -1,9 +1,9 @@
-import { Leftbar } from "@/components/root/leftbar"
+// import { Leftbar } from "@/components/root/leftbar"
 
 export default function InterviewQuestionLayout({ children }) {
     return (
         <div className="flex items-start gap-14">
-            <Leftbar key="leftbar" />
+            {/* <Leftbar key="leftbar" /> */}
             <div className="flex-[4]">{children}</div>{" "}
         </div>
     )
